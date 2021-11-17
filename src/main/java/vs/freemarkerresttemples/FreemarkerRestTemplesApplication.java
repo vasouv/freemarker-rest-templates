99ba696ab7041c24d2de1b5454f605dffc3219e0
vsouvatz <vassilis.souvatzis@intrasoft-intl.com>
2021-11-17 20:27:13 +0200

    project setup

---
+++ src/main/java/vs/freemarkerresttemples/FreemarkerRestTemplesApplication.java
@@ -0,0 +1,13 @@
+package vs.freemarkerresttemples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerRestTemplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarkerRestTemplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vs/freemarkerresttemples/FreemarkerRestTemplesApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.freemarkerresttemples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarkerRestTemplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
